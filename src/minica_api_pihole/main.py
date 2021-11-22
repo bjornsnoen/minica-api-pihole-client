4e66e085f90b1ab564de02c2fb93a18bc8feafa4
Bjørn Snoen <bjorn@fink.no>
2021-11-22 21:25:03 +0100

    (feat) Systemd install/uninstall

--- src/minica_api_pihole/main.py
+++ src/minica_api_pihole/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from dataclasses import dataclass
-from os import sync
+from os import getuid, sync
 from pathlib import Path
 from shutil import move, which
 from subprocess import run
@@ -77,6 +77,79 @@ def main(host: str, port: int, topic: str):
     client.on_message = on_message
 
     client.loop_forever()
+
+
+@click.command(
+    help="Install and enable a systemd unit for the minica sync program with the given host, port and topic"
+)
+@click.argument("host")
+@click.option("-p", "--port", default=1883, help="The MQTT port to connect to")
+@click.option("-t", "--topic", default="domains", help="The MQTT topic to listen on")
+def install(host: str, port: int, topic: str):
+    systemd_unit_file = Path("/etc/systemd/system/minicasync.service")
+    if systemd_unit_file.exists():
+        print(f"Systemd unit service already installed at {str(systemd_unit_file)}")
+        exit(1)
+
+    if getuid() != 0:
+        print("Can't install unless you're root, rerun the program with sudo")
+        exit(2)
+
+    bin = which("minica-pihole-sync")
+    if not bin:
+        print("minica-pihole-sync not found in $PATH, you need to install this as root")
+        print("\t$ pip install minica-api-pihole-client")
+        exit(3)
+
+    unit_contents = f"""
+    [Unit]
+    Description=Sync traefik minica domain events to pihole dns
+    After=network-online.target
+    Wants=network-online.target
+    
+    [Service]
+    User=root
+    ExecStart={bin} --port {port} --topic {topic} {host}
+    Restart=always
+    
+    [Install]
+    WantedBy=multi-user.target
+    """
+    lines = list(map(lambda l: l.strip(), filter(None, unit_contents.split("\n"))))
+    print(f"Installing the following to {systemd_unit_file}")
+    print("\n".join(lines))
+
+    with systemd_unit_file.open("w") as fp:
+        fp.write("\n".join(lines))
+    sync()
+    print(f"Installed service file at {str(systemd_unit_file)}")
+
+    proc = run(
+        ["systemctl", "enable", "--now", systemd_unit_file.name], capture_output=True
+    )
+    if proc.returncode > 0:
+        print(proc.stderr)
+        exit(proc.returncode)
+    else:
+        print("Enabled service")
+
+
+@click.command(help="Uninstall the systemd unit for the minica sync listener")
+def uninstall():
+    if getuid() != 0:
+        print("Can't uninstall unless you're root, rerun the program with sudo")
+        exit(1)
+
+    systemd_unit_file = Path("/etc/systemd/system/minicasync.service")
+    if not systemd_unit_file.exists():
+        print("Systemd unit file not installed")
+        exit(2)
+
+    print("Disabling systemd service")
+    run(["systemctl", "disable", "--now", systemd_unit_file.name], capture_output=True)
+    print("Removing systemd service unit file")
+    systemd_unit_file.unlink()
+    print("Done")
 
 
 if __name__ == "__main__":
